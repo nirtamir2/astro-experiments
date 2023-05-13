08ce54cfb999d770cc724328a9a59a675ecbf05a
Nir Tamir <nirtamir2@gmail.com>
2023-05-13 13:56:50 +0300

    add react integration and create counters in solid and react

--- src/components/react/Counter.tsx
+++ src/components/react/Counter.tsx
@@ -5,6 +5,6 @@ import {useState} from "react";
 export function Counter() {
     const [count, setCount] = useState(0);
     return (
-        <button onClick={() => setCount((count) => count + 1)}>{count}</button>
+        <button className="p-3 rounded bg-gray-200 border hover:bg-gray-300" type="button" onClick={() => setCount((count) => count + 1)}>{count}</button>
     );
 }
--- src/components/solid/Counter.tsx
+++ src/components/solid/Counter.tsx
@@ -5,6 +5,7 @@ import {createSignal} from "solid-js";
 export function Counter() {
     const [count, setCount] = createSignal(0);
     return (
-        <button onClick={() => setCount(count => count + 1)}>{count()}</button>
+        <button class="p-3 rounded bg-gray-200 border hover:bg-gray-300" type="button"
+                onClick={() => setCount(count => count + 1)}>{count()}</button>
     );
 }
